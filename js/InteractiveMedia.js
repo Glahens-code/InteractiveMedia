4795a71b3fd737d4db78b497ecacde4127a403e1
Glahens-code <70273355+Glahens-code@users.noreply.github.com>
2020-09-12 16:39:28 -0400

    Added Animals

---
+++ js/InteractiveMedia.js
@@ -0,0 +1,30 @@
+d3.select("#Dragon")
+.on("mouseenter", function(){
+    d3.select("#message")
+    .text("He Would be Great he raised Michael Bradshaw, Who He teaches Computer Science at Centre")
+} )
+d3.select("#Dragon")
+.on("mouseleave", function(){
+    d3.select("#message")
+    .text(" ")
+} )
+d3.select("#Cat")
+.on("mouseenter", function(){
+    d3.select("#message")
+    .text("He ate Carol Baskins's husband in a past life")
+} )
+d3.select("#Cat")
+.on("mouseleave", function(){
+    d3.select("#message")
+    .text(" ")
+} )
+d3.select("#pony")
+.on("mouseenter", function(){
+    d3.select("#message")
+    .text("absoloutley GREAT for Bronies")
+} )
+d3.select("#pony")
+.on("mouseleave", function(){
+    d3.select("#message")
+    .text(" ")
+} )
